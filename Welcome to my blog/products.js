2c6b5ef9a47fd5858b2e3f8cb22266c37b942997
Hassin9 <100482155+Hassin9@users.noreply.github.com>
2022-05-14 13:37:17 +0100

    Add files via upload

---
+++ Welcome to my blog/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "SOLID COURSES",
+    price: 259,
+    instock: 100,
+    description:
+      "Solidworks courses for CSWA Certification.",
+    imgSrc: "./img/t1.png",
+  },
+  {
+    id: 1,
+    name: "CATIA COURSES",
+    price: 199,
+    instock: 43,
+    description:
+      "Basic Courses for part design .",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "ABAQUS COURSES",
+    price: 329.99,
+    instock: 10,
+    description:
+      "Constraint simulation with ABAQUS.",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "ISO 9001 COURSES",
+    price: 149,
+    instock: 5,
+    description:
+      "Introduction to iSO 9001 introduction.",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "IATF COURSES",
+    price: 499,
+    instock: 4,
+    description:
+      "Difference between isO 9001 and IaTF Certification.",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "QHSE COURSES",
+    price: 300,
+    instock: 40,
+    description:
+      "Introduction to integrated Quality management Systems ",
+    imgSrc: "./img/t6.png",
+  },
+];
